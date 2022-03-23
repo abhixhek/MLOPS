c3ce532493b1145c3a49f4e59cd04053e7dcfe9c
Abhishek Kothari <abhishek.k.kothari@oracle.com>
2022-03-23 17:09:16 +0530

    added setup.py & Distributions

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.0",
+    description="Wine Package",
+    author="Abhishek Kothari",
+    packages=find_packages(),
+    license="MIT"
+)
