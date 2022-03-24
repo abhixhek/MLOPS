acea31abb9d3363350f23c64b46c5dc1a619f9a4
Abhishek Kothari <abhishek.k.kothari@oracle.com>
2022-03-24 15:28:39 +0530

    added workflow for CML

--- build/lib/tests/test_config.py
+++
@@ -1,4 +0,0 @@
-def test_generic() :
-    a = 30
-    b = 40
-    assert a != b
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,17 @@
+import pytest
+
 def test_generic() :
     a = 30
     b = 40
     assert a != b
+
+class NotInRange(Exception):
+    def __init__(self, message = "Value Not in Range"):
+        self.message = message
+        super().__init__(self.message)
+
+def test_generic1():
+    a = 5
+    with pytest.raises(ValueError):
+        if a not in range(10,20):
+            raise NotInRange
